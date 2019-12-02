ce991e467c1b839a3ad493c9b156c0155fbb2557
John Hellbom <john@johnhellbom.com>
2019-12-02 20:41:59 +0100

    Brute force implementation...

---
+++ day-2/index.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const intCode = require("./intcode");
+
+const file = fs.readFileSync("input.txt").toString();
+const numbers = file.split(",");
+
+const maxIterations = 10000;
+const targetResult = 19690720;
+const nounMax = Math.sqrt(maxIterations);
+const verbMax = Math.sqrt(maxIterations);
+
+const input = calculateInputForOutput(targetResult);
+if (input) {
+  console.log(
+    `Noun: ${input.noun} Verb: ${input.verb} Result: ${100 * input.noun +
+      input.verb}`
+  );
+} else {
+  console.log("Could not calculate input.");
+}
+
+function calculateInputForOutput(expectedResult) {
+  for (let noun = 0; noun < nounMax; noun++) {
+    for (let verb = 0; verb < verbMax; verb++) {
+      if (computeResult(noun, verb) === expectedResult) {
+        return {
+          verb,
+          noun
+        };
+      }
+    }
+  }
+
+  return null;
+}
+
+function computeResult(noun, verb) {
+  intCode.initialize(numbers, noun, verb);
+  intCode.compute();
+  return intCode.result;
+}
---
+++ day-2/intcode.js
@@ -0,0 +1,66 @@
+const EXIT_CODE = 99;
+
+const OPERATIONS = {
+  1: add,
+  2: multiply
+};
+
+let result = undefined;
+let numbers = [];
+let pointer = 0;
+
+function initialize(input, noun, verb) {
+  result = undefined;
+  pointer = 0;
+  numbers = [...input];
+  numbers[1] = +noun;
+  numbers[2] = +verb;
+}
+
+function compute() {
+  while (processInstruction()) {
+    step();
+  }
+
+  this.result = numbers[0];
+}
+
+function processInstruction() {
+  const opCode = numbers[pointer];
+
+  if (opCode === EXIT_CODE) {
+    return false;
+  }
+
+  const address1 = numbers[pointer + 1];
+  const address2 = numbers[pointer + 2];
+  const outputAddress = numbers[pointer + 3];
+
+  const param1 = +numbers[address1];
+  const param2 = +numbers[address2];
+
+  if (OPERATIONS[opCode]) {
+    const output = OPERATIONS[opCode](param1, param2);
+    numbers[outputAddress] = output;
+  } else {
+    return false;
+  }
+
+  return pointer < numbers.length;
+}
+
+function add(input1, input2) {
+  return input1 + input2;
+}
+
+function multiply(input1, input2) {
+  return input1 * input2;
+}
+
+function step() {
+  pointer += 4;
+}
+
+exports.initialize = initialize;
+exports.compute = compute;
+exports.result = result;
